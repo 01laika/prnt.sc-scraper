9a012fe744bc7ebadef375790e6966646aed6674
laika <71929285+01laika@users.noreply.github.com>
2021-01-04 21:37:04 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+from random import randint
+import random
+import requests
+import string
+import time
+
+user_agent = {"User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}
+letters = string.ascii_lowercase
+delay = float(input("delay per each request sent?\t=> "))
+
+while True:
+    iden = ( ''.join(random.choice(letters)for i in range(randint(3, 11))))
+    url = f"http://prnt.sc/{iden}"
+    time.sleep(delay)
+    reqs = requests.get(url, headers=user_agent) 
+    soup = BeautifulSoup(reqs.text, 'html.parser') 
+    for title in soup.find_all('title'): 
+        if title.get_text() == "Lightshot — screenshot tool for Mac & Win":
+           print("invalid link")
+        else:
+            if title.get_text() == "Screenshot by Lightshot":
+                print(f"fonud valid link {url}\nsaved to working.txt")
+                f = open("found.txt", "a")
+                f.write(f"working url\t==>  {url}\n")
